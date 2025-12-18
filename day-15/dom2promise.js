2a89b50113d0d2cdfcd9b2a1d344d0a7d23b5c90
Barsharani Panigrahi <panigrahibarsharani20@gmail.com>
2025-12-18 12:26:29 +0530

    uploading file

---
+++ day-15/dom2promise.js
@@ -0,0 +1,100 @@
+console.log("today we learn")
+// document.addEventListener("keydown",()=>{
+//     console.log("key down")
+// })
+// document.addEventListener("keydown",(e)=>{
+//     console.log(e);
+// })
+// document.addEventListener("keydown",(e)=>{
+//     console.log(e.key);
+//     console.log(e.code);
+//     console.log(e.keyCode);
+//     console.log(e.Ctrlkey);
+//     console.log(e.altKey);
+//     console.log(e.shiftKey);
+// })
+// document.addEventListener("keyup",()=>{
+//     console.log("key up")
+// })
+// document.addEventListener("keypress",()=>{
+//         console.log("key press")
+// })
+// let input=document.querySelector("#input-tag");
+// input.addEventListener("input",()=>{
+//     console.log("input event");
+// })
+// let input=document.querySelector("#input-tag");
+// input.addEventListener("input",(e)=>{
+//     console.log("e");
+// })
+// let change=document.querySelector("#input-tag");
+// change.addEventListener("change",()=>{
+//     console.log("change event");
+// })
+// let focus=document.querySelector("#input-tag");
+// focus.addEventListener("focus",()=>{
+//     console.log("focus event");
+// })
+// let blur=document.querySelector("#input-tag");
+// blur.addEventListener("blur",()=>{
+//     console.log("blur event");
+// })
+// let form=document.querySelector("form");
+// console.log("form");
+// form.addEventListener("submit",()=>{
+//     console.log("submit")
+// });
+
+// let form=document.querySelector("form");
+// console.log("form");
+// form.addEventListener("submit",(evl)=>{
+//     evl.preventDefault()
+//     console.log("submit")
+//     console.log(evl)
+// });
+// //window
+// window.addEventListener("load",()=>{
+//     console.log("load")
+// });
+// window.addEventListener("load",(e)=>{
+//         console.log("load")
+//         console.log(e)
+//     });
+// window.addEventListener("DOMContentLoaded",()=>{
+//     console.log("dom load")
+// })
+window.addEventListener("DOMContentLoaded",(e)=>{
+        console.log("dom load")
+        console.log(e)
+    })
+
+// window.addEventListener("resize",()=>{
+//     console.log("resize")
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
